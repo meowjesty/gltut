9c35eca4118796e99cb373654521e8ca37c0cc15
meowjesty <meowjesty_nyan@outlook.com>
2020-12-18 06:38:11 -0300

    Added a bunch of cubes to get a better visualization of the camera deal.
    This looks more like an issue with
    the projection matrix than with the
    camera itself.

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,8 +3,9 @@ use std::{
     time::Instant,
 };
 
+use log::info;
 use renderer::{Camera, Projection, Uniforms, World};
-use vertex::Vertex;
+use vertex::{cube, Vertex};
 use wgpu::util::DeviceExt;
 use winit::{
     dpi,
@@ -64,8 +65,8 @@ fn compute_position_offsets(elapsed: f32) -> (f32, f32) {
     let loop_duration = 5.0;
     let scale = PI * 2.0 / loop_duration;
     let current_time_through = elapsed % loop_duration;
-    let x_offset = f32::cos(current_time_through * scale) * 0.005;
-    let y_offset = f32::sin(current_time_through * scale) * 0.005;
+    let x_offset = f32::cos(current_time_through * scale) * 0.055;
+    let y_offset = f32::sin(current_time_through * scale) * 0.055;
     (x_offset, y_offset)
 }
 
@@ -144,9 +145,23 @@ impl CameraController {
         camera.position += forward * (self.forward - self.backward) * self.speed * delta_time;
         camera.position += right * (self.right - self.left) * self.speed * delta_time;
 
+        // Move in/out (aka. "zoom")
+        // Note: this isn't an actual zoom. The camera's position
+        // changes when zooming. I've added this to make it easier
+        // to get closer to an object you want to focus on.
+        let (pitch_sin, pitch_cos) = camera.pitch.sin_cos();
+        let scrollward =
+            glam::Vec3::new(pitch_cos * yaw_cos, pitch_sin, pitch_cos * yaw_sin).normalize();
+        camera.position += scrollward * self.scroll * self.speed * self.sensitivity * delta_time;
+        self.scroll = 0.0;
+
         // Move up/down. Since we don't use roll, we can just
         // modify the y coordinate directly.
         camera.position.y += (self.up - self.down) * self.speed * delta_time;
+
+        // Rotate
+        camera.yaw += self.rotate_horizontal * self.sensitivity * delta_time;
+        camera.pitch += -self.rotate_vertical * self.sensitivity * delta_time;
 
         self.rotate_horizontal = 0.0;
         self.rotate_vertical = 0.0;
@@ -168,8 +183,12 @@ fn handle_input(event: &DeviceEvent, world: &mut World, delta_time: f32) {
         }) => {
             world.camera_controller.process_keyboard(*key, *state);
         }
-        DeviceEvent::MouseMotion { delta } => {}
-        DeviceEvent::MouseWheel { delta } => {}
+        DeviceEvent::MouseMotion { delta } => {
+            world.camera_controller.process_mouse(delta.0, delta.1);
+        }
+        DeviceEvent::MouseWheel { delta } => {
+            world.camera_controller.process_scroll(delta);
+        }
         DeviceEvent::Motion { axis, value } => {}
         DeviceEvent::Button { button, state } => {}
         DeviceEvent::Text { codepoint } => {}
@@ -196,185 +215,74 @@ fn main() {
         .build(&event_loop)
         .unwrap();
 
+    let (mut vertices, mut indices) = cube(
+        glam::Vec3::new(5.0, 5.0, 0.0),
+        5.0,
+        0,
+        glam::Vec3::new(1.0, 0.0, 0.0),
+    );
+
+    let (mut high_z_vertices, mut high_z_indices) = cube(
+        glam::Vec3::new(-10.0, -10.0, 10.0),
+        5.0,
+        24,
+        glam::Vec3::new(0.0, 1.0, 1.0),
+    );
+
+    let (mut neg_z_vertices, mut neg_z_indices) = cube(
+        glam::Vec3::new(10.0, 10.0, -10.0),
+        5.0,
+        48,
+        glam::Vec3::new(0.0, 1.0, 1.0),
+    );
+
+    let (mut zeroed_vertices, mut zeroed_indices) = cube(
+        glam::Vec3::new(0.0, 0.0, 0.0),
+        5.0,
+        72,
+        glam::Vec3::new(0.0, 1.0, 1.0),
+    );
+
+    vertices.append(&mut high_z_vertices);
+    vertices.append(&mut neg_z_vertices);
+    vertices.append(&mut zeroed_vertices);
+    indices.append(&mut high_z_indices);
+    indices.append(&mut neg_z_indices);
+    indices.append(&mut zeroed_indices);
+
+    for i in 1..12 {
+        let (mut vs, mut is) = cube(
+            glam::Vec3::new(
+                5.0 + (i as f32 * 5.0),
+                5.0 + (i as f32 * 5.0),
+                5.0 + (i as f32 * 5.0),
+            ),
+            5.0,
+            72 + (i * 24),
+            glam::Vec3::new(0.0, 1.0, 1.0),
+        );
+
+        vertices.append(&mut vs);
+        indices.append(&mut is);
+    }
+
     let mut world = World {
         // NOTE(alex): Position is done in counter-clockwise fashion, starting from the middle point
         // in this case.
-        vertices: vec![
-            // Rotating triangle
-            // 0
-            Vertex {
-                position: glam::const_vec3!([0.4, 0.4, 0.5]), // middle point
-                color: glam::const_vec3!([1.0, 0.0, 0.0]),
-                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
-            },
-            // 1
-            Vertex {
-                position: glam::const_vec3!([-0.4, 0.4, 0.5]), // left-most point
-                color: glam::const_vec3!([0.0, 1.0, 0.0]),
-                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
-            },
-            // 2
-            Vertex {
-                position: glam::const_vec3!([0.4, -0.4, 0.5]), // right-most point
-                color: glam::const_vec3!([0.0, 0.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
-            },
-            // 3
-            Vertex {
-                position: glam::const_vec3!([-0.4, -0.4, 0.5]),
-                color: glam::const_vec3!([1.0, 0.0, 0.0]),
-                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
-            },
-            // 4
-            Vertex {
-                position: glam::const_vec3!([-0.7, 0.9, 0.0]),
-                color: glam::const_vec3!([1.0, 0.0, 0.0]),
-                texture_coordinates: glam::const_vec2!([0.4131759, 0.00759614]),
-            },
-            // 5
-            Vertex {
-                position: glam::const_vec3!([-0.9, 0.7, 0.0]),
-                color: glam::const_vec3!([1.0, 0.0, 0.0]),
-                texture_coordinates: glam::const_vec2!([0.0048659444, 0.43041354]),
-            },
-            // 6
-            Vertex {
-                position: glam::const_vec3!([-0.5, 0.7, 0.0]),
-                color: glam::const_vec3!([1.0, 0.0, 0.0]),
-                texture_coordinates: glam::const_vec2!([0.28081453, 0.949397057]),
-            },
-            // 7
-            Vertex {
-                position: glam::const_vec3!([-0.7, -0.7, 0.0]),
-                color: glam::const_vec3!([0.0, 1.0, 0.0]),
-                texture_coordinates: glam::const_vec2!([0.85967, 0.84732911]),
-            },
-            // 8
-            Vertex {
-                position: glam::const_vec3!([-0.9, -0.9, 0.0]),
-                color: glam::const_vec3!([0.0, 1.0, 0.0]),
-                texture_coordinates: glam::const_vec2!([0.9414737, 0.2652641]),
-            },
-            // 9
-            Vertex {
-                position: glam::const_vec3!([-0.5, -0.9, 0.0]),
-                color: glam::const_vec3!([0.0, 1.0, 0.0]),
-                texture_coordinates: glam::const_vec2!([0.9414737, 0.2652641]),
-            },
-            // 10
-            Vertex {
-                position: glam::const_vec3!([0.7, 0.9, 1.0]),
-                color: glam::const_vec3!([0.0, 0.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.4131759, 0.00759614]),
-            },
-            // 11
-            Vertex {
-                position: glam::const_vec3!([0.5, 0.7, 1.0]),
-                color: glam::const_vec3!([0.0, 0.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.0048659444, 0.43041354]),
-            },
-            // 12
-            Vertex {
-                position: glam::const_vec3!([0.9, 0.7, 1.0]),
-                color: glam::const_vec3!([0.0, 0.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.28081453, 0.949397057]),
-            },
-            // 13
-            Vertex {
-                position: glam::const_vec3!([0.7, -0.7, 0.5]),
-                color: glam::const_vec3!([1.0, 0.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.85967, 0.84732911]),
-            },
-            // 14
-            Vertex {
-                position: glam::const_vec3!([0.5, -0.9, 0.5]),
-                color: glam::const_vec3!([1.0, 0.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.9414737, 0.2652641]),
-            },
-            // 15
-            Vertex {
-                position: glam::const_vec3!([0.9, -0.9, 0.5]),
-                color: glam::const_vec3!([1.0, 0.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.9414737, 0.2652641]),
-            },
-            // 16
-            Vertex {
-                position: glam::const_vec3!([-0.086, 0.492, 0.0]),
-                color: glam::const_vec3!([1.0, 1.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.85967, 0.84732911]),
-            },
-            // 17
-            Vertex {
-                position: glam::const_vec3!([-0.495, 0.0695, 0.0]),
-                color: glam::const_vec3!([1.0, 1.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.9414737, 0.2652641]),
-            },
-            // 18
-            Vertex {
-                position: glam::const_vec3!([0.441, 0.234, 0.0]),
-                color: glam::const_vec3!([1.0, 0.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.9414737, 0.2652641]),
-            },
-            // 19
-            Vertex {
-                position: glam::const_vec3!([0.0, 0.9, 0.0]),
-                color: glam::const_vec3!([1.0, 1.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.85967, 0.84732911]),
-            },
-            // 20
-            Vertex {
-                position: glam::const_vec3!([-0.9, -0.9, 0.0]),
-                color: glam::const_vec3!([1.0, 1.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.9414737, 0.2652641]),
-            },
-            // 21
-            Vertex {
-                position: glam::const_vec3!([0.9, -0.9, 0.0]),
-                color: glam::const_vec3!([1.0, 0.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.9414737, 0.2652641]),
-            },
-            // 22
-            Vertex {
-                position: glam::const_vec3!([1.0, 1.0, 0.0]),
-                color: glam::const_vec3!([0.0, 0.0, 0.0]),
-                texture_coordinates: glam::const_vec2!([0.85967, 0.84732911]),
-            },
-            // 23
-            Vertex {
-                position: glam::const_vec3!([-1.0, -1.0, 0.0]),
-                color: glam::const_vec3!([0.0, 0.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.9414737, 0.2652641]),
-            },
-            // 24
-            Vertex {
-                position: glam::const_vec3!([1.0, -1.0, 0.0]),
-                color: glam::const_vec3!([1.0, 1.0, 1.0]),
-                texture_coordinates: glam::const_vec2!([0.9414737, 0.2652641]),
-            },
-            // 25
-            Vertex {
-                position: glam::const_vec3!([-1.0, 1.0, 0.0]),
-                color: glam::const_vec3!([1.0, 0.0, 0.0]),
-                texture_coordinates: glam::const_vec2!([0.9414737, 0.2652641]),
-            },
-        ],
+        vertices,
         // NOTE(alex): These indices will work from:
         // middle->left->right (implicit back to middle);
         // middle->right->up left (implicit back to middle);
-        #[rustfmt::skip]
-        indices: vec![
-            // Rotating triangle
-            0, 1, 2, 3, 2, 1, // Rotating square
-            4, 5, 6, // Top-left triangle
-            7, 8, 9, // Bottom-left triangle
-            10, 11, 12, // Top-right triangle
-            13, 14, 15, // Bottom-right triangle
-            16, 17, 18, // Where
-            // 19, 20, 21, // GIANT
-            // 22, 23, 24, 25, 23, 22, // SQUARE
-        ],
+        indices,
         // TODO(alex): Get a better understanding of this camera, triangles disappeared because
         // I don't truly understand the values here.
+        // TODO(alex): Camera is still wrong, mouse rotation is busted, it jiggles like crazy,
+        // without really rotating. The math is still off, that's why WASD are rotating the camera,
+        // instead of moving it sideways.
+        // The camera position itself is working as intended regarding WASD:
+        // - (AD) changes x;
+        // - (Shift/Space) changes y;
+        // - (WS) changes z (the more forward you go, the lower z gets, until it flips negative);
         camera: Camera {
             // eye: (0.0, 1.0, 2.0).into(),
             // target: (0.0, 0.0, 0.0).into(),
@@ -383,7 +291,7 @@ fn main() {
             // fov_y: 45.0,
             // z_near: 0.1,
             // z_far: 100.0,
-            position: glam::const_vec3!([0.0, 0.0, 5.0]),
+            position: glam::const_vec3!([0.0, 0.0, 10.0]),
             // NOTE(alex): Yaw "rotates" around the Y-axis, positive rotates the right hand to the
             // right (meaning the thumb points away from the nose), negative rotates the thumb
             // towards your nose (-90 degrees makes your thumb point to your nose, the Z-axis
@@ -405,7 +313,7 @@ fn main() {
     };
 
     let mut renderer = futures::executor::block_on(renderer::Renderer::new(&window, &mut world));
-    let mut step_timer = fixedstep::FixedStep::start(75.0).limit(5);
+    let mut step_timer = fixedstep::FixedStep::start(30.0).limit(5);
     window.request_redraw();
 
     event_loop.run(move |event, _target_window, control_flow| {
@@ -436,7 +344,15 @@ fn main() {
                 _ => (),
             },
             Event::MainEventsCleared => {
-                while step_timer.update() {}
+                // TODO(alex): How to properly interpolate the values after update?
+                while step_timer.update() {
+                    let (x_offset, y_offset) =
+                        compute_position_offsets(timer.elapsed().as_secs_f32());
+                    for mut vertex in world.vertices.iter_mut() {
+                        vertex.position.x += x_offset;
+                        vertex.position.y += y_offset;
+                    }
+                }
                 world
                     .camera_controller
                     .update_camera(&mut world.camera, step_timer.render_delta() as f32);
@@ -444,11 +360,6 @@ fn main() {
                     .uniforms
                     .update_view_projection(&world.camera, &world.projection);
                 let delta = step_timer.render_delta();
-                let (x_offset, y_offset) = compute_position_offsets(timer.elapsed().as_secs_f32());
-                for mut vertex in world.vertices.iter_mut() {
-                    vertex.position.x += x_offset * delta as f32;
-                    vertex.position.y += y_offset * delta as f32;
-                }
                 // TODO(alex): Do the rotation and setup the MVP.
                 // world.camera.model = glam::const_mat4!([1.0; 16]);
                 window.request_redraw();
@@ -458,6 +369,7 @@ fn main() {
                 pool.run_until_stalled();
             }
             Event::RedrawRequested { .. } => {
+                info!("Camera position {:?}", world.camera);
                 renderer.present(&world, &spawner);
             }
             _ => (),
--- app/src/vertex.rs
+++ app/src/vertex.rs
@@ -29,7 +29,7 @@ impl Vertex {
     };
 
     // pub fn descriptor_3d<'x>() -> Box<wgpu::VertexBufferDescriptor<'x>> {
-    //     let attributes = wgpu::vertex_attr_array![0 => Float3, 1 => Float3];
+    //     let mut attributes = wgpu::vertex_attr_array![0 => Float3, 1 => Float3];
     //     Box::new(wgpu::VertexBufferDescriptor {
     //         stride: core::mem::size_of::<Self>() as wgpu::BufferAddress,
     //         step_mode: wgpu::InputStepMode::Vertex,
@@ -45,3 +45,308 @@ impl Vertex {
     //     }
     // }
 }
+
+pub fn cube(
+    origin: glam::Vec3,
+    size: f32,
+    index: u32,
+    color: glam::Vec3,
+) -> (Vec<Vertex>, Vec<u32>) {
+    let mut back = vec![
+        Vertex {
+            position: glam::Vec3::new(origin.x, origin.y, origin.z),
+            color: glam::Vec3::new(1.0, 0.0, 0.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x + size, origin.y, origin.z),
+            color: glam::Vec3::new(1.0, 0.0, 0.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x, origin.y + size, origin.z),
+            color: glam::Vec3::new(1.0, 0.0, 0.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x + size, origin.y + size, origin.z),
+            color: glam::Vec3::new(1.0, 0.0, 0.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+    ];
+    let mut back_indices = vec![
+        0 + index,
+        1 + index,
+        2 + index,
+        3 + index,
+        2 + index,
+        1 + index,
+    ];
+
+    let mut front = vec![
+        Vertex {
+            position: glam::Vec3::new(origin.x, origin.y, origin.z + size),
+            color: glam::Vec3::new(0.0, 0.0, 1.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x + size, origin.y, origin.z + size),
+            color: glam::Vec3::new(0.0, 0.0, 1.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x, origin.y + size, origin.z + size),
+            color: glam::Vec3::new(0.0, 0.0, 1.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x + size, origin.y + size, origin.z + size),
+            color: glam::Vec3::new(0.0, 0.0, 1.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+    ];
+    let mut front_indices = vec![
+        0 + 4 + index,
+        1 + 4 + index,
+        2 + 4 + index,
+        3 + 4 + index,
+        2 + 4 + index,
+        1 + 4 + index,
+    ];
+
+    let mut left = vec![
+        Vertex {
+            position: glam::Vec3::new(origin.x, origin.y, origin.z),
+            color: glam::Vec3::new(color.x, color.y, color.z + size),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x, origin.y, origin.z + size),
+            color: glam::Vec3::new(color.x, color.y, color.z + size),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x, origin.y + size, origin.z),
+            color: glam::Vec3::new(color.x, color.y, color.z + size),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x, origin.y + size, origin.z + size),
+            color: glam::Vec3::new(color.x, color.y, color.z + size),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+    ];
+    let mut left_indices = vec![
+        0 + 8 + index,
+        1 + 8 + index,
+        2 + 8 + index,
+        3 + 8 + index,
+        2 + 8 + index,
+        1 + 8 + index,
+    ];
+
+    let mut right = vec![
+        Vertex {
+            position: glam::Vec3::new(origin.x + size, origin.y, origin.z),
+            color: glam::Vec3::new(1.0, 0.0, 0.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x + size, origin.y, origin.z + size),
+            color: glam::Vec3::new(0.0, 1.0, 0.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x + size, origin.y + size, origin.z),
+            color: glam::Vec3::new(0.0, 0.0, 1.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x + size, origin.y + size, origin.z + size),
+            color: glam::Vec3::new(0.0, 1.0, 1.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+    ];
+    let mut right_indices = vec![
+        0 + 12 + index,
+        1 + 12 + index,
+        2 + 12 + index,
+        3 + 12 + index,
+        2 + 12 + index,
+        1 + 12 + index,
+    ];
+
+    let mut top = vec![
+        Vertex {
+            position: glam::Vec3::new(origin.x, origin.y + size, origin.z + size),
+            color: glam::Vec3::new(0.0, 0.0, 0.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x + size, origin.y + size, origin.z + size),
+            color: glam::Vec3::new(0.0, 0.0, 0.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x, origin.y + size, origin.z),
+            color: glam::Vec3::new(0.0, 0.0, 0.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x + size, origin.y + size, origin.z),
+            color: glam::Vec3::new(0.0, 0.0, 0.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+    ];
+    let mut top_indices = vec![
+        0 + 16 + index,
+        1 + 16 + index,
+        2 + 16 + index,
+        3 + 16 + index,
+        2 + 16 + index,
+        1 + 16 + index,
+    ];
+
+    let mut bottom = vec![
+        Vertex {
+            position: glam::Vec3::new(origin.x, origin.y, origin.z + size),
+            color: glam::Vec3::new(1.0, 1.0, 1.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x + size, origin.y, origin.z + size),
+            color: glam::Vec3::new(1.0, 1.0, 1.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x, origin.y, origin.z),
+            color: glam::Vec3::new(1.0, 1.0, 1.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+        Vertex {
+            position: glam::Vec3::new(origin.x + size, origin.y, origin.z),
+            color: glam::Vec3::new(1.0, 1.0, 1.0),
+            texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+        },
+    ];
+    let mut bottom_indices = vec![
+        0 + 20 + index,
+        1 + 20 + index,
+        2 + 20 + index,
+        3 + 20 + index,
+        2 + 20 + index,
+        1 + 20 + index,
+    ];
+
+    back.append(&mut front);
+    back.append(&mut left);
+    back.append(&mut right);
+    back.append(&mut top);
+    back.append(&mut bottom);
+
+    back_indices.append(&mut front_indices);
+    back_indices.append(&mut left_indices);
+    back_indices.append(&mut right_indices);
+    back_indices.append(&mut top_indices);
+    back_indices.append(&mut bottom_indices);
+
+    (back, back_indices)
+}
+
+/*
+vertices: vec![
+            // Back square
+            Vertex {
+                position: glam::const_vec3!([-0.6, 0.6, 0.0]),
+                color: glam::const_vec3!([1.0, 1.0, 1.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            Vertex {
+                position: glam::const_vec3!([-0.6, -0.6, 0.0]),
+                color: glam::const_vec3!([1.0, 1.0, 1.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            Vertex {
+                position: glam::const_vec3!([0.6, -0.6, 0.0]),
+                color: glam::const_vec3!([1.0, 1.0, 1.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            Vertex {
+                position: glam::const_vec3!([0.6, 0.6, 0.0]),
+                color: glam::const_vec3!([1.0, 1.0, 1.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            // Front square
+            Vertex {
+                position: glam::const_vec3!([-0.6, 0.6, 0.6]),
+                color: glam::const_vec3!([0.0, 0.0, 0.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            Vertex {
+                position: glam::const_vec3!([-0.6, -0.6, 0.6]),
+                color: glam::const_vec3!([0.0, 0.0, 0.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            Vertex {
+                position: glam::const_vec3!([0.6, -0.6, 0.6]),
+                color: glam::const_vec3!([0.0, 0.0, 0.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            Vertex {
+                position: glam::const_vec3!([0.6, 0.6, 0.6]),
+                color: glam::const_vec3!([0.0, 0.0, 0.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            // Left square
+            Vertex {
+                position: glam::const_vec3!([-0.61, 0.6, 0.0]),
+                color: glam::const_vec3!([1.0, 0.0, 0.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            Vertex {
+                position: glam::const_vec3!([-0.61, -0.6, 0.0]),
+                color: glam::const_vec3!([1.0, 0.0, 0.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            Vertex {
+                position: glam::const_vec3!([-0.61, -0.6, 0.6]),
+                color: glam::const_vec3!([1.0, 0.0, 0.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            Vertex {
+                position: glam::const_vec3!([-0.61, 0.6, 0.6]),
+                color: glam::const_vec3!([1.0, 0.0, 0.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            // Right square
+            Vertex {
+                position: glam::const_vec3!([0.61, 0.6, 0.0]),
+                color: glam::const_vec3!([0.0, 1.0, 0.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            Vertex {
+                position: glam::const_vec3!([0.61, -0.6, 0.0]),
+                color: glam::const_vec3!([0.0, 1.0, 0.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            Vertex {
+                position: glam::const_vec3!([0.61, -0.6, 0.6]),
+                color: glam::const_vec3!([0.0, 1.0, 0.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+            Vertex {
+                position: glam::const_vec3!([0.61, 0.6, 0.6]),
+                color: glam::const_vec3!([0.0, 1.0, 0.0]),
+                texture_coordinates: glam::const_vec2!([0.0, 0.0]),
+            },
+        ],
+
+#[rustfmt::skip]
+        indices: vec![
+            0, 1, 2, 3, 0, 2, // Rotating square
+            4, 5, 6, 7, 4, 6, // Rotating square
+            8, 9, 10, 11, 8, 10, // Rotating square
+            12, 13, 14, 15, 12, 14, // Rotating square
+            16, 17, 18,
+        ],
+*/
